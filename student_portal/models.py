fdbf63bf0f26bc50991601348ffd45f228cc1faf
Jacob Cook <102179895+Js-cook@users.noreply.github.com>
2022-05-14 19:34:17 +0000

    ADD - Created database models and performed database migration

--- student_portal/models.py
+++ student_portal/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Announcement(models.Model):
+  title = models.CharField(max_length=200)
+  date = models.DateField(default=timezone.now)
+  contents = models.CharField(max_length=2000)
+  sender = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class MeetingMinute(models.Model):
+  date = models.DateField(default=timezone.now)
+  contents = models.CharField(max_length=3000)
+  
