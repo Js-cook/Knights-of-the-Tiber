ad63778fda0019f1f8345b898b21fbe424a22837
Jacob Cook <102179895+Js-cook@users.noreply.github.com>
2022-05-14 19:23:19 +0000

    FIX - Fixed links to different pages within student portal

--- student_portal/urls.py
+++ student_portal/urls.py
@@ -5,7 +5,7 @@ app_name = "student_portal"
 
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
-    path("announcements/", views.announcements, name="announcements"),
-    path("cursus-honorum/", views.cursus, name="cursus_honorum"),
-    path("meeting-minutes/", views.minutes, name="minutes")
+    path("announcements", views.announcements, name="announcements"),
+    path("cursus-honorum", views.cursus, name="cursus_honorum"),
+    path("meeting-minutes", views.minutes, name="minutes")
 ]
