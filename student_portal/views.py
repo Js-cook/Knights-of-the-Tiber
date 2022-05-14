5947a02ec28efe1f2ec9307091ba0bd35102dcee
Jacob Cook <102179895+Js-cook@users.noreply.github.com>
2022-05-14 18:52:50 +0000

    ADD - More UI changes and additions to the student portal section

--- student_portal/views.py
+++ student_portal/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard(request):
   return render(request, "student_portal/dashboard.html")
+
+def announcements(request):
+  return render(request, "student_portal/announcements.html")
+
+def minutes(request):
+  return render(request, "student_portal/minutes.html")
+
+def cursus(request):
+  return render(request, "student_portal/cursus.html")
