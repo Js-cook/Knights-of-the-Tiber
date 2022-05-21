8cb0264578cdfa77123f3ed76385eeef1877307d
Jacob Cook <102179895+Js-cook@users.noreply.github.com>
2022-05-21 02:23:27 +0000

    ADD - UI changes and updates

--- student_portal/views.py
+++ student_portal/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-
+from .models import Announcement, MeetingMinute
 # Create your views here.
 def dashboard(request):
   return render(request, "student_portal/dashboard.html")
 
+# Prolly going to remove this page and add it to dashboard
 def announcements(request):
   return render(request, "student_portal/announcements.html")
 
 def minutes(request):
-  return render(request, "student_portal/minutes.html")
+  minutes = MeetingMinute.objects.all()
+  return render(request, "student_portal/minutes.html", {"minutes": minutes})
 
 def cursus(request):
   return render(request, "student_portal/cursus.html")
