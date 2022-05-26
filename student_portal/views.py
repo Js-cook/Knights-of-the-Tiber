4cb722c3393ca17ac9d8410b181dab259677e428
Jacob Cook <102179895+Js-cook@users.noreply.github.com>
2022-05-26 23:37:11 +0000

    REMOVE - Removed announcements page and links and added it to dashboard

--- student_portal/views.py
+++ student_portal/views.py
@@ -3,14 +3,14 @@ from .models import Announcement, MeetingMinute
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-@login_required
+# @login_required
 def dashboard(request):
   announcements = Announcement.objects.all()
   return render(request, "student_portal/dashboard.html", {"announcements": announcements})
   
 # Prolly going to remove this page and add it to dashboard
-def announcements(request):
-  return render(request, "student_portal/announcements.html")
+# def announcements(request):
+#   return render(request, "student_portal/announcements.html")
 
 @login_required
 def minutes(request):
