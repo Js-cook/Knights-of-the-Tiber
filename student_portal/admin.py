f8aee0e10a8eef5060f4c195afb1126763602f45
Jacob C <jscook1001@gmailcom>
2022-05-27 17:53:09 -0400

    ADD - UI changes and setup stuff

--- student_portal/admin.py
+++ student_portal/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import MeetingMinute, Announcement
 
 # Register your models here.
+admin.site.register(MeetingMinute)
+admin.site.register(Announcement)
