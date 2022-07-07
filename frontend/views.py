15ae0037ac2f77f4854221d1f225ce2ccb6f388a
Jacob C <jsc1001@yahoo.com>
2022-07-06 20:24:13 -0400

    ADD - Added officer listing to homepage

--- frontend/views.py
+++ frontend/views.py
@@ -3,7 +3,11 @@ from student_portal.models import Consul, Praetor, Quaestor, Aedile
 
 # Create your views here.
 def index(request):
-  return render(request, "frontend/index.html")
+  consuls = Consul.objects.all()
+  praetors = Praetor.objects.all()
+  quaestors = Quaestor.objects.all()
+  aediles = Aedile.objects.all()
+  return render(request, "frontend/index.html", {"consuls": consuls, "praetors": praetors, "quaestors": quaestors, "aediles": aediles})
 
 def constitution(request):
   return render(request, "frontend/const.html")
